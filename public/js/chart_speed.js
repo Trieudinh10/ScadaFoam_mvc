14899a38e8203d9d42cc4db9f5c3d489cd75d214
PQH tobi <phanquochung2511@gmail.com>
2023-07-03 12:37:17 +0700

    .

---
+++ public/js/chart_speed.js
@@ -0,0 +1,41 @@
+   // Chuẩn bị dữ liệu
+   const dataSpeed = {
+    labels: ['1h', '2h', '3h', '4h', '5h'],
+    datasets: [
+      {
+        label: 'Điều Hòa 1',
+        data: [23, 28, 20, 19, 30],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      },
+      {
+        label: 'Điều Hòa 2',
+        data: [14, 28, 16, 30, 17],
+        fill: false,
+        borderColor: 'rgb(75, 192, 00)',
+        tension: 0.1
+      },
+      {
+        label: 'Điều Hòa 3',
+        data: [31, 10, 20, 19, 18],
+        fill: false,
+        borderColor: 'rgb(75, 100, 192)',
+        tension: 0.1
+      }
+    ]
+  };
+  // Tạo biểu đồ
+  const Speed = document.getElementById('chartSpeed').getContext('2d');
+  const chartSpeed = new Chart(Speed, {
+    type: 'line',
+    data: dataSpeed,
+    options: {
+      responsive: true,
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
