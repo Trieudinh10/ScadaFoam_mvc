2a6ce18349b3ff7bacb21c2495b5480961d0681e
PQH tobi <phanquochung2511@gmail.com>
2023-07-12 09:06:05 +0700

    add block send token

--- public/js/chart_room.js
+++ public/js/chart_room.js
@@ -3,26 +3,19 @@
     labels: ['1h', '2h', '3h', '4h', '5h','1h', '2h', '3h', '4h', '5h'],
     datasets: [
       {
-        label: 'Điều Hòa 1',
+        label: 'Nhiệt Độ',
         data: [23, 28, 20, 19, 30,23, 28, 20, 19, 30],
         fill: false,
         borderColor: 'rgb(75, 192, 192)',
         tension: 0.1
       },
       {
-        label: 'Điều Hòa 2',
+        label: 'Độ Ẩm',
         data: [14, 28, 16, 30, 17,14, 28, 16, 30, 17],
         fill: false,
         borderColor: 'rgb(75, 192, 00)',
         tension: 0.1
       },
-      {
-        label: 'Điều Hòa 3',
-        data: [31, 10, 20, 19, 18,31, 10, 20, 19, 18],
-        fill: false,
-        borderColor: 'rgb(75, 100, 192)',
-        tension: 0.1
-      }
     ]
   };
   // Tạo biểu đồ
