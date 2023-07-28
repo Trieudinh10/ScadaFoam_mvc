3ff4235a8a8de5058d7df31ae17b3a6a847be6a3
Dinh Le Quang Trieu <dinhlequangtrieu@CMC.THACO>
2023-07-28 14:53:55 +0700

    initial

---
+++ public/js/FC6_alarm.js
@@ -0,0 +1,44 @@
+// Chương trình con đọc dữ liệu SQL
+function fn_Alarm_Show(){
+    socket.emit("msg_Alarm_Show", "true");
+}
+function fn_Alarm_Show_Display()
+{
+    socket.on('Alarm_Show',function(data){
+        fn_table_Alarm(data);
+    }); 
+}
+// Chương trình con hiển thị SQL ra bảng
+function fn_table_Alarm(data){
+    if(data){
+        $("#table_Alarm tbody").empty(); 
+        var len = data.length;
+        var txt = "<tbody>";
+        if(len > 0){
+            for(var i=0;i<len;i++){
+                    txt += "<tr><td>"+data[i].date_time
+                        +"</td><td>"+data[i].ID
+                        +"</td><td>"+data[i].Status
+                        +"</td><td>"+data[i].AlarmName
+                        +"</td></tr>";
+                    }
+            if(txt != ""){
+            txt +="</tbody>"; 
+            $("#table_Alarm").append(txt);
+            }
+        }
+    }   
+}
+// Tìm kiếm cảnh báo theo thời gian
+function fn_Alarm_By_Time()
+{
+    var val = [document.getElementById('dtpk_AL_Search_Start').value,
+               document.getElementById('dtpk_AL_Search_End').value];
+    socket.emit('msg_Alarm_ByTime', val);
+}
+function fn_Alarm_By_Time_Display()
+{
+    socket.on('Alarm_ByTime', function(data){
+        fn_table_Alarm(data); // Show alarm
+    });
+}
